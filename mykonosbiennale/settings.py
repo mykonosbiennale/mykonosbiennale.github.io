126946b1be788c84acfc0c7f3740b6361346a9e6
thanos <thanosv@gmail.com>
2019-09-14 11:51:35 -0400

    renamed db

--- mykonosbiennale/settings.py
+++ mykonosbiennale/settings.py
@@ -163,7 +163,7 @@ TEMPLATES = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'mb.sqlite3'),
     }
 }
 # DATABASES = {
